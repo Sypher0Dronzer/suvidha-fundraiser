75bccc6e132b6f43bb3ae16bc505a329c5f0d35b
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2023-11-12 19:58:36 +0530

    last changes

--- script.js
+++ script.js
@@ -223,14 +223,42 @@ setInterval(function () {
   }
 }, 500);
 
+// navbar animation 
 
-//about us animation
-window.addEventListener('scroll',()=>{
-  var aboutUsLocation = document
-  .querySelector("#about-us")
-  .getBoundingClientRect().top;
-  if (scrollY > aboutUsLocation){
-    gsap.fromTo(".abt-us-left", {x:-500,opacity:0}, {x:0,opacity:1, duration:2});
-    
-  }
+gsap.from("nav img", {
+  y:-400,
+  opacity:0,
+  duration:0.7,
+})
+gsap.from(".nav-item", {
+  y:-400,
+  opacity:0,
+  stagger:0.5
+
+})
+gsap.from(".nav-items .btn-div", {
+  scale:0,
+  opacity:0,
+  stagger:0.5
+})
+
+// titl box animation 
+gsap.from(".title-box h1",{
+  opacity:0,
+  x:-800,
+  duration:.8
+})
+gsap.from(".title-box h2",{
+  opacity:0,
+  x:800,
+  duration:.8
+})
+gsap.from(".title-box p",{
+  opacity:0,
+  y:400,
+  duration:.8
+})
+gsap.from(".title-box .btn-div a,.title-box .btn-div button",{
+  opacity:0,
+  duration:1.5,
 })
